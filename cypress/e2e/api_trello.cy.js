d40aa6025f560743d6648fceefbcb1b21bf6c4f4
Lislaine Tavares <lis.tavares@gmail.com>
2026-06-13 11:51:35 -0300

    Ajustes name

--- cypress/e2e/api_trello.cy.js
+++ cypress/e2e/api_trello.cy.js
@@ -1,7 +1,7 @@
 describe('API Trello', () => {
   const actionId = '592f11060f95a3d3d46a987a'
 
-  it('valida status e campo name via comando customizado', () => {
+  it('Validar status e campo name da action do Trello', () => {
     cy.getTrelloAction(actionId).then(({ status, body }) => {
       expect(status).to.equal(200)
 
